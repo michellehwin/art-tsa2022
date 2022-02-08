d082b07f522791e0b5df2f4c9a87f724525a5391
ken-myers <kennmyersiii@gmail.com>
2022-02-08 10:40:34 -0600

    add basic modal

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
-$('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 160,
-    gutter: 20
+$(document).ready(function(){
+    init();
 });
+
+
+function init(){
+
+    // MicroModal.init()
+
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 160,
+        gutter: 20
+    });
+
+    $(".modal-container").click(function(e){
+        e.stopPropagation();
+    });
+
+    $('.modal').click(function(){
+        $('.modal').addClass('hidden');
+    });
+
+    $('.painting').each(function(index){
+        title = $(this).attr("ptitle");
+        artist = $(this).attr("partist");
+        style = $(this).attr("pstyle");
+
+        $(this).click(function(){
+            $(".modal-title").text(title);
+            $(".modal-content").html(
+                "Artist: "+artist+"<br>"+
+                "Style: "+style);
+            $(".modal").removeClass("hidden");
+        });
+    })
+}
